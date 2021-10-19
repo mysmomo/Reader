7c51ee7b33ad824d428d1dae59d51b9eb385cdb5
mys_MacBook Pro <1299953247@qq.com>
2021-10-19 21:00:12 +0800

    Initial Commit

---
+++ Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reader
+//
+//  Created by mys_momo on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reader/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Reader
+//
+//  Created by mys_momo on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
