30569ae33fb05803b9b67e486f52ef9fa3b8e376
mys_MacBook Pro <1299953247@qq.com>
2021-10-23 21:33:23 +0800

    dark and light

--- Reader/ContentView.swift
+++ Reader/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     //将视图和数据模型关联起来，并接受published更新
     @StateObject var publishedData = DataValue()
+    @State var darkMode = false
     
     var body: some View {
         NavigationView{
@@ -17,8 +18,17 @@ struct ContentView: View {
                 NavigationLink(destination: Detail(article: article)) {
                     Row(article: article)
                 }
+            }.navigationTitle("编辑推荐")
+        }.toolbar {
+            Button {
+                darkMode.toggle()
+            } label: {
+                Image(systemName: darkMode ? "sun.max.fill" : "moon.fill")
+                    .font(.system(.headline, design: .rounded))
             }
-        }.navigationTitle("编辑推荐")
+
+        }
+        .preferredColorScheme(darkMode ? .dark : .light)
     }
 }
 
